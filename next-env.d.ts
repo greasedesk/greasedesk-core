3aae8d208d9cae5a27f4b95bcb4e0002e3904cb6
greasedesk <mr.iain.gunn@gmail.com>
2025-10-27 21:54:19 +0000

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,9 @@
+/**
+ * File: next-env.d.ts
+ * Last edited: 2025-10-27 21:55 Europe/London
+ */
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited.
